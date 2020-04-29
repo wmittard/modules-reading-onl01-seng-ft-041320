5243fceb820ec74ba6d25fe05a5ad4884c191c35
William Mittard <billymittard@ymail.com>
2020-04-29 21:03:26 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid 
+  attr_accessor :name 
+  
+  def intialize(name) 
+    @name = name 
+    extend MetaDancing
+  end
+  end 
